cf4c68bf029ce8fde995cfbb86c02e3bdcca5943
SpcFORK <126259962+SpcFORK@users.noreply.github.com>
2024-02-10 02:20:43 +0000

    Wam

---
+++ app/app/error/entry.js
@@ -0,0 +1,22 @@
+window.Main = class Main {
+
+  // ** THIS IS A TEMPLATE ** //
+
+  // This class automatically inherits:
+  //   - The entry element,
+  //   - The page pathname,
+  //   - The file location.
+
+  // this.entry, this.path, this.entryPath,
+
+  static PageTitle = 'Main';
+
+  static async preload() {
+
+  }
+
+  constructor(entry) {
+
+  }
+
+}
